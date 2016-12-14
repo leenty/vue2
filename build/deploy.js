fa09241035c416927ed4b2f88e746faac37395d7
leenty <leenty@qq.com>
2016-12-14 23:19:19 +0800

    尝试新方式部署

--- build/deploy.js
+++ build/deploy.js
@@ -1,5 +1,9 @@
 require('shelljs/global')
 let ora = require('ora')
+var git = require("nodegit")
+var path = require('path')
+
+var argv = require('minimist')(process.argv.slice(2))
 
 Date.prototype.format = function(fmt) {
   let o = {   
@@ -11,9 +15,9 @@ Date.prototype.format = function(fmt) {
     "q+" : Math.floor((this.getMonth()+3)/3),
     "S"  : this.getMilliseconds()
   }
-  if(/(y+)/.test(fmt))   
+  if(/(y+)/.test(fmt))
     fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));   
-  for(let k in o)   
+  for(let k in o)
     if(new RegExp("("+ k +")").test(fmt))   
   fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ?
     (o[k]) :
@@ -21,42 +25,65 @@ Date.prototype.format = function(fmt) {
   return fmt
 } 
 
-const deployName = new Date().format("yyyy-MM-dd hh:mm:ss")
-
-// const repertoryUrl = 'git@git.coding.net:leenty/vue2.leenty.com.git'
+const commitTime = new Date()
+const commitName = `deploy at ${commitTime.format("yyyy-MM-dd hh:mm:ss")}`
+console.log(commitTime, commitTime.getTime());
+// console.log(argv)
 // cd('.deploy')
-// exec(`git remote origin set-url ${repertoryUrl}`)
-// const checkoutRepertory = function () {
-//   exec(`git clone -b git@github.com:leenty/vue2.git gh-pages`)
-//   // git clone -b <branch name> [remote repository address]
+// let test = exec('git status')
+// console.log(test)
+
+// const setDeploy = function () {
+//   let dir = '.deploy'
+//   let spinner = ora(`building and deploy at ${argv._.toString} \n `)
+//   spinner.start()
+
+//   rm('-rf', `${dir}/*`)
+//   cp('-R', ['dist/CNAME', 'dist/index.html', 'dist/static', 'dist/article', 'dist/demo', dir])
+//   cd(dir)
+//   exec('git add .')
+//   exec(`git cm -m "deploy at ${deployName}"`)
+//   cd('..')
+
+//   spinner.stop()
+//   console.log('\n')
 // }
 
-const deploy = function (branch, repository) {
-  let dir = '.deploy'
-  let spinner = ora(`building and deploy at ${repository} \n `)
-  spinner.start()
+// const pushDeploy = function (branch, repository) {
+//   exec(`git push -u ${repository} HEAD:${branch} --force`)
+// }
 
-  // rm('-rf', [`${dir}/CNAME`, `${dir}/index.html`, `${dir}/static`, `${dir}/article`, `${dir}/demo`])
-  // rm('-rf', [`${dir}/CNAME`, `${dir}/index.html`, `${dir}/static`])
-  // cp('-R', ['dist/CNAME', 'dist/index.html', 'dist/static', dir])
-  rm('-rf', dir)
-  exec(`git clone -b ${branch} ${repository} ${dir}`)
-  rm('-rf', `${dir}/*`)
-  cp('-R', ['dist/CNAME', 'dist/index.html', 'dist/static', 'dist/article', 'dist/demo', dir])
-  cd(dir)
-  exec('git add .')
-  exec(`git cm -m "deploy at ${deployName}"`)
-  exec(`git pull --rebase origin ${branch}`)
-  exec(`git push origin ${branch}`)
-  cd('..')
+let repo
+let index
+let oid
+const relativePath = '../.deploy/'
+git.Repository.open('.deploy')
+  .then(repoResult => (repo = repoResult, repo.refreshIndex()))
+  .then(indexResult => (index = indexResult, repo.getStatus()))
+  .then(statuses => {
+    // console.log(statuses[0])
+    if (statuses.length > 0) {
+      statuses.forEach(file => {
+        console.log(path.join(__dirname, relativePath, file.path()))
+        index.addByPath(file.path())
+          .then(() => index.write())
+          .then(() => index.writeTree())
+          .then(oidResult => (oid = oidResult, git.Reference.nameToId(repo, "HEAD")))
+          .then(head => repo.getCommit(head))
+          .then(parent => {
+            var author = git.Signature.default(repo)
+            return repo.createCommit("HEAD", author, author, commitName, oid, [parent])
+          })
+          .then(console.log)
+      })
+    }
+  })
 
-  spinner.stop()
-  console.log('\n')
-}
-if (argv._.length > 0) {
-  argv._.forEach(k => deploy(repos[k].branch, repos[k].repo))
-} else {
-  for (let k in repos) {
-    deploy(repos[k].branch, repos[k].repo)
-  }
-}
+
+// if (argv._.length > 0) {
+//   argv._.forEach(k => pushDeploy(repos[k].branch, repos[k].repo))
+// } else {
+//   for (let k in repos) {
+//     pushDeploy(repos[k].branch, repos[k].repo)
+//   }
+// }
